b3af0f17de7ab819aa8d1777f92a3d50ac10e5d9
luca-marote <l184698@dac.unicamp.br>
2025-11-18 13:44:31 -0300

    Add files via upload

---
+++ pages/1-authors.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+st.markdown(
+    '<div class="title">Uncovering Flash Droughts in Brazil through Global and Regional Perspectives: A Systematic Literature Review </div>', unsafe_allow_html=True)
+    
+    st.markdown('<div class="authors"> P. C. M. Vasconcelos<sup>a</sup>,\
+                L. C. D. Marote<sup>a</sup>, \
+                G. A. J. Bimbatti<sup>a</sup>,\
+                G. C. Gesualdo<sup>b</sup>, \
+                N. Vergopolan<sup>c</sup>, \
+                E. Wendland<sup>d</sup>, \
+                P. T. S. Oliveira<sup>e</sup>, A. Mishra<sup>f</sup> &  M. C. Lucas<sup>a</sup> </div>', unsafe_allow_html=True)
+                
+                
+    
+    #AFILIAÇÕES
+    
+    st.markdown("<br><br> ", unsafe_allow_html=True)
+    
+    st.markdown('<div class="subtitle">a: School of Technology at Universidade Estadual de Campinas, Limeira, Brazil</div>', unsafe_allow_html=True)
+    st.markdown('<div class="subtitle">b: Department of Meteorology and Atmospheric Science, Pennsylvania State University, Pennsylvania, United States</div>', unsafe_allow_html=True)
+    st.markdown('<div class="subtitle">c: Department of Earth, Environmental and Planetary Sciences Department, Rice University, Houston, United States </div>', unsafe_allow_html=True)
+    st.markdown('<div class="subtitle">d: School of Engineering, Universidade de São Paulo, São Carlos, Brazil</div>', unsafe_allow_html=True)
+    st.markdown('<div class="subtitle">e: School of Engineering, Universidade Federal de Mato Grosso do Sul, Campo Grande, Brazil</div>', unsafe_allow_html=True)
+    st.markdown('<div class="subtitle">f: Zachry Department of Civil and Environmental Engineering; Texas A&M University, College Station, United States</div>', unsafe_allow_html=True)  
+    
+    st.markdown("<br> ", unsafe_allow_html=True)
+    
+    
+    st.markdown('<div class="subtitle"><b>Corresponding author:</b> Priscila Vasconcelos (priscila.costa91@hotmail.com)</div>', unsafe_allow_html=True)
+    
+    
+    #KEYPOINTS
+    st.markdown('<h4 style="margin-top:40px;">🔑 Key Points:</h4>', unsafe_allow_html=True)
+    
+    st.markdown("""
+    <div class="keypoints">
+    • Recognized by a sudden reduction in soil moisture at the beginning of a drought event, flash droughts challenge food, energy, and water security.<br>
+    • Lack of a clear definition limits comparisons, early warning efforts, and the ability to create policies for flash droughts.<br>
+    • Recent projected climate change indicates growing vulnerability in Brazil, reinforcing its status as a global hotspot and underscoring the need for region-specific flash droughts detection criteria.
+    </div>
+    """, unsafe_allow_html=True)
+    
+
+    
+    
+    #FIGURE 1
+    st.markdown('<div class="legend"> Comparison between slow-onset (a) and flash (b) droughts highlighting their key characteristics.\
+                On the y-axis, SM is soil moisture, P is precipitation, and ET is evapotranspiration.', unsafe_allow_html=True) 
+    
+    st.image("images/Sudden decline in soil moisture in a few days.png")
