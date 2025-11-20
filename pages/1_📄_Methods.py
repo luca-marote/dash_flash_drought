16fa63459682e79234823b27ee0f816796a26904
luca-marote <l184698@dac.unicamp.br>
2025-11-20 19:56:01 -0300

    Update 1_📄_Methods.py

--- pages/1_📄_Methods.py
+++ pages/1_📄_Methods.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+hsj_logo = 'https://www.tandfonline.com/cms/asset/3e7614cf-7643-4a3d-9bad-f444bd55e5c7/thsj20.v070.i14.cover.jpg'
+t_f_logo = 'https://www.informascope.com/views/default/_images/_logos/urunler/taylor_and_francis.png'
+
+st.sidebar.image(hsj_logo, use_column_width=True)
+st.sidebar.image(t_f_logo, use_column_width=True)
+
 st.header("🧪 Methodology")  # anchor removido
     
 st.markdown("<br> ", unsafe_allow_html=True)
