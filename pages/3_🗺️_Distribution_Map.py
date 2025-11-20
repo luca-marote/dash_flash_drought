d6435cd9e1c1e468cc7ca5f379a9f579afeb781a
luca-marote <l184698@dac.unicamp.br>
2025-11-20 20:14:08 -0300

    Update 3_🗺️_Distribution_Map.py

--- pages/3_🗺️_Distribution_Map.py
+++ pages/3_🗺️_Distribution_Map.py
@@ -4,8 +4,9 @@ import streamlit.components.v1 as components
 hsj_logo = 'https://www.tandfonline.com/cms/asset/3e7614cf-7643-4a3d-9bad-f444bd55e5c7/thsj20.v070.i14.cover.jpg'
 t_f_logo = 'https://www.informascope.com/views/default/_images/_logos/urunler/taylor_and_francis.png'
 
-st.sidebar.image(hsj_logo, use_column_width=True)
+
 st.sidebar.image(t_f_logo, use_column_width=True)
+st.sidebar.image(hsj_logo, use_column_width=True)
 
 link_url = "https://review-fd-map.netlify.app/"
 
