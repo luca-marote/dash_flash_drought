b291d613bee8f35bed3aa695fef26e8a4440e2e4
luca-marote <l184698@dac.unicamp.br>
2025-11-20 19:56:59 -0300

    Update 4_🔥_Drought_Analysis.py

--- pages/4_🔥_Drought_Analysis.py
+++ pages/4_🔥_Drought_Analysis.py
@@ -11,6 +11,12 @@ from folium.plugins import MarkerCluster
 import plotly.graph_objects as go
 import plotly.express as px
 
+hsj_logo = 'https://www.tandfonline.com/cms/asset/3e7614cf-7643-4a3d-9bad-f444bd55e5c7/thsj20.v070.i14.cover.jpg'
+t_f_logo = 'https://www.informascope.com/views/default/_images/_logos/urunler/taylor_and_francis.png'
+
+st.sidebar.image(hsj_logo, use_column_width=True)
+st.sidebar.image(t_f_logo, use_column_width=True)
+
 st.header("🌡️ Drought Analysis")  # anchor removido
     
 tab1, tab2 = st.tabs(["Geographic Distribution", "Indicators/Variables"])
